c5b90b454a257ee127327b1532fd7ecbc5c87ff9
Eduardo Braun <eduardo@rushers.org>
2018-10-31 04:04:24 -0300

    Add create user button to login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,6 +16,7 @@
                 </v-form>
               </v-card-text>
               <v-card-actions>
+                <v-btn block color="primary" :to="'signon'" :loading="loading">New User</v-btn>
                 <v-spacer></v-spacer>
                 <v-btn block color="primary" @click="login" :loading="loading">Login</v-btn>
               </v-card-actions>
